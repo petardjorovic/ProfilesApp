eab7a2ab90bceb75924031b030bd621e9b6ae940
petardjorovic <perodjorovic@gmail.com>
2025-01-03 20:11:35 +0100

    Correction in database/index.js

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mysql2 = require('mysql2');
 
-const db = mysql2.createConnection({
+const db = mysql2.createPool({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASS,
